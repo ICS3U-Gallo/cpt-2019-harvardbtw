859352645ef113cfc7dce3e43fa1204951fa1b09
ClarenceCorpuz <47527535+ClarenceCorpuz@users.noreply.github.com>
2019-12-30 22:15:14 -0500

    Update chapter_3.py
    Aiming, Base, Red Dot

--- chapter_3.py
+++ chapter_3.py
@@ -6,20 +6,26 @@ import os
 WIDTH = 1000
 HEIGHT = 700
 
-
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
+        global finish, bullet_speed
 
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.BLACK)
+        self.set_mouse_visible(False)
+        self.total_time = 30.0
+        finish = False
 
         self.player = arcade.Sprite(center_x=WIDTH/2, center_y=10)
         self.player.texture = arcade.make_soft_square_texture(50, arcade.color.ASH_GREY, outer_alpha=255)
 
-        self.mouse = arcade.Sprite(center_x=100, center_y=100)
-        self.mouse.texture = arcade.make_soft_square_texture(50, arcade.color.BLUE, outer_alpha=255)
+        self.base = arcade.Sprite(center_x=WIDTH/2, center_y=-475)
+        self.base.texture = arcade.make_soft_square_texture(WIDTH, arcade.color.BATTLESHIP_GREY, outer_alpha=255)
 
-        self.enemy_texture = arcade.make_soft_square_texture(50, arcade.color.RED, outer_alpha=255)
+        self.mouse = arcade.Sprite(center_x=100, center_y=100)
+        self.mouse.texture = arcade.make_soft_circle_texture(10, arcade.color.RED, outer_alpha=10)
+
+        self.enemy_texture = arcade.make_soft_circle_texture(50, arcade.color.GREEN, outer_alpha=255)
         self.enemies = arcade.SpriteList()
 
         # create an enemy
@@ -31,42 +37,64 @@ class MyGame(arcade.Window):
             enemy.texture = self.enemy_texture
             self.enemies.append(enemy)
 
-        self.laser_texture = arcade.make_soft_square_texture(30, arcade.color.ORANGE, outer_alpha=255)
-        self.lasers = arcade.SpriteList()
+        self.bullet_texture = arcade.make_soft_square_texture(10, arcade.color.ORANGE, outer_alpha=255)
+        self.bullets = arcade.SpriteList()
+        bullet_speed = 15
 
     def on_draw(self):
+        global finish
         arcade.start_render()
 
+        self.enemies.draw()
+        self.base.draw()
         self.player.draw()
         self.mouse.draw()
-        self.enemies.draw()
-        self.lasers.draw()
+        self.bullets.draw()
+
+#Timer
+        minutes = int(self.total_time) // 60
+        seconds = int(self.total_time) % 60
+        output = f"Time: {minutes:02d}:{seconds:02d}"
+
+        if seconds == 0:
+            finish = True
+            arcade.draw_text("Mission Complete", 10, HEIGHT-40, arcade.color.WHITE, 30)
+
+        if finish == False:
+            arcade.draw_text(output, 10, HEIGHT-40, arcade.color.WHITE, 30)
 
     def update(self, delta_time):
-        self.lasers.update()
+        global finish
+        self.bullets.update()
         self.enemies.update()
 
-        for laser in self.lasers:
-            laser_kill = arcade.check_for_collision_with_list(laser, self.enemies)
-            if len(laser_kill) > 0:
-                laser.kill()
+# Update Timer
+        self.total_time -= delta_time
+
+
+# Update Bullet
+        for bullet in self.bullets:
+            bullet_kill = arcade.check_for_collision_with_list(bullet, self.enemies)
+            if len(bullet_kill) > 0:
+                bullet.kill()
 
         for enemy in self.enemies:
-            enemy_kill = arcade.check_for_collision_with_list(enemy, self.lasers)
+            enemy_kill = arcade.check_for_collision_with_list(enemy, self.bullets)
             if len(enemy_kill) > 0:
                 enemy.kill()
+
 
     def on_mouse_motion(self, x, y, delta_x, delta_y):
         self.mouse.center_x = x
         self.mouse.center_y = y
 
     def on_mouse_press(self, x, y, button, key_modifiers):
-        laser = arcade.Sprite()
-        laser.center_x = self.player.center_x
-        laser.center_y = self.player.center_y
-        laser.texture = self.laser_texture
-        laser.width = 5
-
+        global bullet_speed
+        bullet = arcade.Sprite()
+        bullet.center_x = self.player.center_x
+        bullet.center_y = self.player.center_y
+        bullet.texture = self.bullet_texture
+        bullet.width = 20
 
         dest_x = x
         dest_y = y
@@ -77,13 +105,13 @@ class MyGame(arcade.Window):
         angle = math.atan2(y_diff, x_diff)
 
 
-        self.laser = math.degrees(angle)
+        bullet.angle = math.degrees(angle)
 
 
-        laser.change_x = math.cos(angle) * 15
-        laser.change_y = math.sin(angle) * 15
+        bullet.change_x = math.cos(angle) * bullet_speed
+        bullet.change_y = math.sin(angle) * bullet_speed
 
-        self.lasers.append(laser)
+        self.bullets.append(bullet)
 
 def main():
     game = MyGame(WIDTH, HEIGHT, "My Game")
@@ -92,4 +120,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
