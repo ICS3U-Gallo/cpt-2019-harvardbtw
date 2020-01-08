dc5d3c5d24914fd94845e8ec03d3ebaa9a7d0670
nlaids <47423885+nlaids@users.noreply.github.com>
2020-01-07 23:32:02 -0500

    Update chapter_2.py

--- chapter_2.py
+++ chapter_2.py
@@ -1,33 +1,60 @@
 
 import arcade
 
-import settings
+WIDTH = 800
+HEIGHT = 600
 
 
-class Chapter2View(arcade.View):
-    def on_show(self):
-        arcade.set_background_color(arcade.color.WHITE)
+class Player:
+    def __init__(self, x, y, x_speed, y_speed, radius, color):
+        self.x = x
+        self.y = y
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+        self.radius = radius
+        self.color = color
+
+    def draw(self):
+        arcade.draw_circle_filled(self.x, self.y, self.radius, self.color)
+
+    def update(self):
+        self.y += self.x_speed
+        self.x += self.y_speed
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.ASH_GREY)
+        self.ball = Player(50, 50, 0, 0, 15, arcade.color.AUBURN)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Chapter 2", settings.WIDTH/2, settings.HEIGHT/2,
-                         arcade.color.BLACK, font_size=30, anchor_x="center")
+        self.ball.draw()
+
+    def update(self, delta_time):
+        self.ball.update()
+
 
     def on_key_press(self, key, modifiers):
-        self.director.next_view()
+        if key == arcade.key.A:
+            self.ball.change_x = -5
+        elif key == arcade.key.D:
+            self.ball.change_x = 5
+        elif key == arcade.key.W:
+            self.ball.change_y = 5
+        elif key == arcade.key.S:
+            self.ball.change_y = -5
 
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.A or key == arcade.key.D:
+            self.ball.change_x = 0
+        elif key == arcade.key.W or key == arcade.key.S:
+            self.ball.change_y = 0
 
-if __name__ == "__main__":
-    """This section of code will allow you to run your View
-    independently from the main.py file and its Director.
-    You can ignore this whole section. Keep it at the bottom
-    of your code.
-    It is advised you do not modify it unless you really know
-    what you are doing.
-    """
-    from utils import FakeDirector
-    window = arcade.Window(settings.WIDTH, settings.HEIGHT)
-    my_view = Chapter2View()
-    my_view.director = FakeDirector(close_on_next_view=True)
-    window.show_view(my_view)
+    def on_mouse_press(self, x, y, button, modifiers):
+        pass
+
+if __name__ == '__main__':
+    MyGame(WIDTH, HEIGHT, "OK")
     arcade.run()
