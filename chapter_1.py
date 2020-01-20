6d2fa1279ab70b235149d67c4828c0a42db4b41e
Danny Lu <danny.lu075@gmail.com>
2020-01-19 20:11:37 -0500

    Update chapter_1.py

--- chapter_1.py
+++ chapter_1.py
@@ -309,7 +309,7 @@ class Chapter1View(arcade.View):
         arcade.set_background_color(arcade.color.LIGHT_GREEN)
 
         # Creates game timer and sets it to 30 seconds
-        self.time = 5
+        self.time = 30.00
 
         # Creates score counter
         self.score = 0
